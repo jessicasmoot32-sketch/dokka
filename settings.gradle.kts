7f1f1543706706c43325162b4cfa8a2dcd72f7f5
jessicasmoot32-sketch <jessicasmoot32@icloud.com>
2026-02-22 02:35:47 -0500

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
-/*
+#/*
  * Copyright 2014-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
 
-rootProject.name = "dokka"
+rootProject.name = ""
 
 pluginManagement {
     includeBuild("build-logic")
